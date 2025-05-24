0005fd8316d0662be54ad69eff6ddf87c94342bf
diego40g <diegopaznaula@hotmail.com>
2025-05-24 19:38:25 +0000

    feat: adding the service to use authentication on firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  async registerWithEmailPassword(email: string, password: string): Promise<void> {
+    await createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async loginWithEmailPassword(email: string, password: string): Promise<void> {
+    await signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async loginWithGoogle(): Promise<void> {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(this.auth, provider);
+  }
+
+  logout(): Promise<void> {
+    return this.auth.signOut();
+  }
+
+  getCurrentUser() {
+    return this.auth.currentUser;
+  }
+
+  onAuthStateChanged() {
+    return from(new Promise(resolve => this.auth.onAuthStateChanged(resolve)));
+  }
 }
