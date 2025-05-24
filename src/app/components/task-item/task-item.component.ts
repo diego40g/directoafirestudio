3cfa0cd0970f7eeb848e682c79cb57592cca111d
diego40g <diegopaznaula@hotmail.com>
2025-05-24 01:33:18 +0000

    feat: adding empty auth-form and task-item

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+  
+}
