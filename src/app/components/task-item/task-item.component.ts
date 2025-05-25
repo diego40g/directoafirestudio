ca8f1a04b23d9f0dd47e5c6ccfcd49de7492d9f8
diego40g <diegopaznaula@hotmail.com>
2025-05-25 22:20:36 +0000

    feat: adding modal to create of update tasks

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,13 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Task } from '../../shared/model/task';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
-  
+  @Input() createdByUserId!: string;
+  @Output() close = new EventEmitter<void>();
+  @Output() taskCreated = new EventEmitter<Task>();
+  task: Task = {
+    createdBy: '', // You'll likely need to set this based on the logged-in user
+    title: '',
+    description: '',
+    invitation: [],
+  };
+
+  constructor(private taskService: TaskService) {}
+
+  invitationString: string = '';
+
+  saveTask() {
+    this.task.invitation = this.invitationString.split(',').map(email => email.trim()).filter(email => email !== '');
+    this.task.createdBy = this.createdByUserId;
+    this.taskService.addTask(this.task).then(() => {
+        this.taskCreated.emit(this.task);
+        this.onClose();
+    }).catch(err => {
+      console.error('Error creating task:', err);
+    });
+  }
+
+  onClose() {
+    this.close.emit();
+  }
 }
