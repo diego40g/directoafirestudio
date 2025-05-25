1c1b2c3ee94a8aaf6cb6fece29ae340a7fc1e36c
diego40g <diegopaznaula@hotmail.com>
2025-05-25 22:20:03 +0000

    feat: adding a services to manager the collection of the store for tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Task } from '../shared/model/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private tasksCollection;
+   
+  constructor(private firestore: Firestore) {
+    this.tasksCollection = collection(this.firestore, 'tasks');
+  }
 
-  constructor() { }
+  getTasks(): Observable<Task []> {
+    return collectionData(this.tasksCollection, { idField: 'id' }) as Observable<Task[]>;
+  }
+
+  addTask(task: Omit<Task, 'id'>): Promise<any> {
+    return addDoc(this.tasksCollection, task);
+  }
+
+  updateTask(task: Task): Promise<void> {
+    const taskDocRef = doc(this.firestore, `tasks/${task.id}`);
+    return updateDoc(taskDocRef, { ...task });
+  }
+
+  deleteTask(taskId: string): Promise<void> {
+    const taskDocRef = doc(this.firestore, `tasks/${taskId}`);
+    return deleteDoc(taskDocRef);
+  }
 }
